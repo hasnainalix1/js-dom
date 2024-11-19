7c16418da25e9d78d2ecd2bc01e7819bb99174db
hasnian1122 <soomronain22@gmail.com>
2024-11-19 04:09:50 -0800

    todo

---
+++ script.js
@@ -0,0 +1,24 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("change", (e) => {
+    document.body.classList.toggle("dark-mode", e.target.checked);
+});
+
+
+let inputs = document.getElementById("input");
+let task = document.querySelector(".task");
+
+function Add() {
+    if (inputs.value === ""){
+        alert ("You need to add someting your list cannot be empty");
+    } else {
+        let newEL = document.createElement("ul") ;
+        newEL.innerHTML = inputs.value + ' <i class = "fa-solid fa-xmark"></i>';
+        task.appendChild(newEL);
+        inputs.value = "";
+        newEL.querySelector("i").addEventListener("click" , remove)
+        function remove(){
+            newEL.remove ();
+        }
+    }
+}
